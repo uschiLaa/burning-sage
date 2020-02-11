750a9251c7187264b8ec89fe9717ebe9f3a6c2a2
Ursula Laa <uschi.laa@gmx.at>
2020-02-12 09:35:15 +1100

    Hollow sphere example added.

--- renderExamples.R
+++ renderExamples.R
@@ -20,7 +20,22 @@ saveGIF(
           max_frames = 100),
   movie.name = "sphere6_xy.gif", interval=0.2)
 
+sp6h <- sphere.hollow(6, 1000)$points
+colnames(sp6h) <- c("a","b","c","d","e","f")
 
+set.seed(2020)
+saveGIF(
+  animate(sp6h,
+          display = display_fisheye(),
+          max_frames = 100),
+  movie.name = "sphere6h_fe.gif", interval=0.2)
+
+set.seed(2020)
+saveGIF(
+  animate(sp6h,
+          display = display_xy( axes="bottomleft"),
+          max_frames = 100),
+  movie.name = "sphere6h_xy.gif", interval=0.2)
 
 data(pollen)
 pollen <- as.matrix(pollen) %>% scale()
