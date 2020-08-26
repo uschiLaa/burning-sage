64d5ab4315f5ce4ace679fe6f8e0ca61d991ae1c
Stuart Lee <lee.s@wehi.edu.au>
2020-08-26 14:50:24 +1000

    update pdfsense example

--- scripts/pdfsense.R
+++ scripts/pdfsense.R
@@ -1,12 +1,14 @@
 # pdfsense example
-library()
+library(burningsage)
 library(tourr)
+library(dplyr)
+library(here)
 data(pdfsense)
 
 
 pcs  <- prcomp(pdfsense[, 7:ncol(pdfsense)])
 
-pdfsense <- dplyr::bind_cols(
+pdfsense <- bind_cols(
   pdfsense, 
   as.data.frame(pcs$x)
 )
@@ -15,3 +17,20 @@ pdfsense$Type <- factor(pdfsense$Type)
 pal <- c("dodgerblue", "darkorange", "darkred")
 col <- pal[as.numeric(pdfsense$Type)]
 
+set.seed(2020)
+render_gif(
+  select(pdfsense, PC1:PC6), 
+  tour_path = grand_tour(),
+  gif_file = here("gifs", "pdfsense_sage.gif"),
+  display = display_sage(col=col, axes = "bottomleft"),
+  frames = 100
+)
+
+set.seed(2020)
+render_gif(
+  select(pdfsense, PC1:PC6), 
+  tour_path = grand_tour(),
+  gif_file = here("gifs", "pdfsense_grand.gif"),
+  display = display_xy(col=col, axes = "bottomleft"),
+  frames = 100
+)
